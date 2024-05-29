d0a5686e2861e76f0279a4fdbfcbff9f50fd4d1c
Bink <tuanbinhnguyenngoc@gmail.com>
2024-05-29 18:16:52 +0700

    complete theme picker and break alarm sound choosing

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,3 +1,36 @@
+// Theme picker
+function setTheme(theme) {
+  document.body.className = "";
+  document.body.classList.add(`theme-${theme}`);
+}
+
+setTheme("sunset");
+
+// Theme button
+const btnTheme = document.querySelectorAll(".btn.btn-theme");
+
+let themeColor;
+btnTheme.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const theme = this.getAttribute("data-theme");
+    document.addEventListener("DOMContentLoaded", setTheme(theme));
+  });
+});
+
+//**********************************************
+//Audio
+const btnRing = document.querySelectorAll(".btn.btn-ring");
+let themeRing;
+btnRing.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const button = event.currentTarget;
+    const audio = button.querySelector("audio");
+    themeRing = document.getElementById(audio.id);
+    console.log(themeRing);
+  });
+});
+// *********************************************
+// Timer and Break counting down (in loop)
 const btnStart = document.querySelector(".btn.start");
 // Timer elements
 const timerTime = document.querySelector(".timer");
@@ -13,10 +46,10 @@ let minBreak = document.querySelector(".minBreak");
 let secBreak = document.querySelector(".secBreak");
 
 let intervalTimer, intervalBreak;
-let min = 25,
+let min = 1,
   sec = 0,
   cnt = 0,
-  breakMinNum = 5,
+  breakMinNum = 1,
   breakSecNum = 0;
 let isPaused = false;
 let isBreak = false;
@@ -71,10 +104,12 @@ function timer() {
     sec--;
   } else if (sec == 0 && min > 0) {
     min--;
-    sec = 59;
+    sec = 10;
   } else if (sec == 0 && min == 0) {
     clearInterval(intervalTimer);
     startBreak();
+    breakMinNum = 1;
+    breakSecNum = 10;
   }
 }
 
@@ -88,19 +123,23 @@ function breakTimer() {
     breakSecNum--;
   } else if (breakSecNum == 0 && breakMinNum > 0) {
     breakMinNum--;
-    breakSecNum = 59;
+    breakSecNum = 10;
   } else if (breakSecNum == 0 && breakMinNum == 0) {
     clearInterval(intervalBreak);
     cnt++;
     startTimer();
+    min = 1;
+    sec = 10;
   }
 }
 
 function startTimer() {
   isTimer = true;
   isBreak = false;
-  document.querySelector(".content").style.backgroundColor =
-    "var(--timeContainer)";
+  const startColor = getComputedStyle(document.documentElement)
+    .getPropertyValue("var(--timeContainer)")
+    .trim();
+  document.querySelector(".content").style.backgroundColor = startColor;
   intervalTimer = setInterval(timer, 1000);
   showTimer();
   hideBreak();
@@ -109,7 +148,13 @@ function startTimer() {
 function startBreak() {
   isTimer = false;
   isBreak = true;
-  document.querySelector(".content").style.backgroundColor = "var(--break)";
+  //play break ringing
+  themeRing.play();
+  // document.querySelector(".content").style.backgroundColor = "#($--break)";
+  const breakColor = getComputedStyle(document.documentElement)
+    .getPropertyValue("var(--break)")
+    .trim();
+  document.querySelector(".content").style.backgroundColor = breakColor;
   intervalBreak = setInterval(breakTimer, 1000);
   showBreak();
   hideTimer();
